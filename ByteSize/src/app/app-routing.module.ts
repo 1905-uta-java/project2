db92a955001219e4e57036d1919f47cf9faad382
mantunguyen <mantunguyen6989@gmail.com>
2019-07-04 18:53:14 -0500

    add page template

---
+++ ByteSize/src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LeaderBoardComponent } from './components/leader-board/leader-board.component';
+import { ProfileComponent } from './components/profile/profile.component';
+
+const routes: Routes = [
+  { 
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'leader-board',
+    component: LeaderBoardComponent
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ ByteSize/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { LeaderBoardComponent } from './components/leader-board/leader-board.component';
+import { ProfileComponent } from './components/profile/profile.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    LeaderBoardComponent,
+    ProfileComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
